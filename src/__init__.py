1aa66230c9b1f3ca9ddc55e175fed737f3ca2f8b
Saket Jha <saketjha0324@gmail.com>
2025-03-27 20:01:32 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+import genre_based_recommendation
+import movie_embeddings_faiss
+import movie_embeddings_faiss
+
+__all__ = [
+    "genre_based_recommendation",
+    "movie_embeddings_faiss",
+    "movie_embeddings_faiss",
+]
