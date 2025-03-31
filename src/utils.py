c46ef531a876d108de801b6fd246f6f96a383ede
Saket Jha <saketjha0324@gmail.com>
2025-03-31 23:47:00 +0530

    create utils.py

---
+++ src/utils.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import ast
+
+def preprocess_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    df['description_length'] = df["overview"].str.split(" ").str.len()
+    df['budget_revenue_ratio'] = df.apply(lambda row: row['revenue'] / row['budget'] if row['budget'] != 0 else None, axis=1)
+    df['vote_share'] = df.apply(lambda row: row['vote_average'] * row['vote_count'], axis=1)
+    df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
+    df['year'] = df['release_date'].dt.year
+    df['years_old'] = abs(df['year'] - 2023)
+    return df
+
+
+def recommend_movies_by_genre(df: pd.DataFrame, genre_query: str, sort_by: str, years_old: int, language: str, k: int) -> pd.DataFrame:
+    def safe_convert(val):
+        if isinstance(val, str):
+            try:
+                return ast.literal_eval(val) if "[" in val else [val]
+            except:
+                return []
+        elif isinstance(val, list):
+            return val
+        else:
+            return []
+    
+    df['genres'] = df['genres'].apply(safe_convert)
+    genre_list = [g.strip() for g in genre_query.split(",")]
+    filtered_df = df[df['genres'].apply(lambda genres: isinstance(genres, list) and any(g in genres for g in genre_list))]
+    filtered_df = filtered_df[filtered_df['years_old'] <= years_old]
+    filtered_df = filtered_df[filtered_df['original_language'] == language]
+    sorted_df = filtered_df.sort_values(by=[sort_by], ascending=False)
+
+    return sorted_df[['id', 'title', 'genres', 'vote_average', 'popularity', 'runtime', 'budget_revenue_ratio']].head(k)
